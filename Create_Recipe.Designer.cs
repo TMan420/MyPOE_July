3ad88762ded61355e04b77f1919001e87654c520
Tauriq <Samuelstauriq@gmail.com>
2023-07-02 19:59:56 +0200

    Added Create Recipe Feature

--- Create_Recipe.Designer.cs
+++ Create_Recipe.Designer.cs
@@ -28,19 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.RecipeNameLabel = new System.Windows.Forms.Label();
+            this.RecipeName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.IngAmount = new System.Windows.Forms.Label();
+            this.IngNumber = new System.Windows.Forms.NumericUpDown();
+            this.BackButton = new System.Windows.Forms.Button();
+            this.EnterButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.IngNumber)).BeginInit();
             this.SuspendLayout();
+            // 
+            // RecipeNameLabel
+            // 
+            this.RecipeNameLabel.AutoSize = true;
+            this.RecipeNameLabel.ForeColor = System.Drawing.Color.Black;
+            this.RecipeNameLabel.Location = new System.Drawing.Point(41, 36);
+            this.RecipeNameLabel.Name = "RecipeNameLabel";
+            this.RecipeNameLabel.Size = new System.Drawing.Size(126, 20);
+            this.RecipeNameLabel.TabIndex = 0;
+            this.RecipeNameLabel.Text = "Name Of Recipe";
+            // 
+            // RecipeName
+            // 
+            this.RecipeName.Location = new System.Drawing.Point(227, 36);
+            this.RecipeName.Name = "RecipeName";
+            this.RecipeName.Size = new System.Drawing.Size(279, 26);
+            this.RecipeName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Name Of Recipe";
+            // 
+            // IngAmount
+            // 
+            this.IngAmount.Location = new System.Drawing.Point(0, 0);
+            this.IngAmount.Name = "IngAmount";
+            this.IngAmount.Size = new System.Drawing.Size(100, 23);
+            this.IngAmount.TabIndex = 9;
+            // 
+            // IngNumber
+            // 
+            this.IngNumber.Location = new System.Drawing.Point(227, 97);
+            this.IngNumber.Maximum = new decimal(new int[] {
+            1215752192,
+            23,
+            0,
+            0});
+            this.IngNumber.Name = "IngNumber";
+            this.IngNumber.Size = new System.Drawing.Size(85, 26);
+            this.IngNumber.TabIndex = 6;
+            this.IngNumber.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // BackButton
+            // 
+            this.BackButton.BackColor = System.Drawing.Color.Red;
+            this.BackButton.Location = new System.Drawing.Point(348, 172);
+            this.BackButton.Name = "BackButton";
+            this.BackButton.Size = new System.Drawing.Size(75, 42);
+            this.BackButton.TabIndex = 7;
+            this.BackButton.Text = "Back";
+            this.BackButton.UseVisualStyleBackColor = false;
+            this.BackButton.Click += new System.EventHandler(this.BackButton_Click);
+            // 
+            // EnterButton
+            // 
+            this.EnterButton.BackColor = System.Drawing.Color.Green;
+            this.EnterButton.Location = new System.Drawing.Point(429, 172);
+            this.EnterButton.Name = "EnterButton";
+            this.EnterButton.Size = new System.Drawing.Size(75, 42);
+            this.EnterButton.TabIndex = 8;
+            this.EnterButton.Text = "Enter";
+            this.EnterButton.UseVisualStyleBackColor = false;
+            this.EnterButton.Click += new System.EventHandler(this.EnterButton_Click);
             // 
             // Create_Recipe
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(559, 226);
+            this.Controls.Add(this.EnterButton);
+            this.Controls.Add(this.BackButton);
+            this.Controls.Add(this.IngNumber);
+            this.Controls.Add(this.IngAmount);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.RecipeName);
+            this.Controls.Add(this.RecipeNameLabel);
             this.Name = "Create_Recipe";
-            this.Text = "Create_Recipe";
+            this.Text = "Create A Recipe";
+            this.Load += new System.EventHandler(this.Create_Recipe_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.IngNumber)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label RecipeNameLabel;
+        private System.Windows.Forms.TextBox RecipeName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label IngAmount;
+        private System.Windows.Forms.NumericUpDown IngNumber;
+        private System.Windows.Forms.Button BackButton;
+        private System.Windows.Forms.Button EnterButton;
     }
 }
--- Create_Recipe.cs
+++ Create_Recipe.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Media;
+using System.Xml;
 
 namespace MyRecipeApplicationPOE
 {
@@ -15,6 +18,45 @@ namespace MyRecipeApplicationPOE
         public Create_Recipe()
         {
             InitializeComponent();
+        }
+
+        private void Create_Recipe_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UpButton_Click(object sender, EventArgs e)
+        {
+            int Clicked = 0;
+            Clicked++;
+            IngAmount.Text = "King";
+        }
+
+        private void BackButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            MainWindow x = new MainWindow();
+            x.Show();
+        }
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            var xx = RecipeName.Text.ToString();
+
+            if (xx.Length.Equals(0))
+            {
+                RecipeNameLabel.Text = "Please Enter A name!";
+            }
+
+            else
+            {                
+                for (int i = 0; i < IngNumber.Value; i++)
+                {
+                    Create_Ingredients x = new Create_Ingredients();
+                    this.Close();
+                    x.Show();
+                }
+            }
         }
     }
 }
